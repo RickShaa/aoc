1acc5b6ce4729134968e16748ffb9e962c8faa8a
RickShaa <r.shaffer@gmx.de>
2022-12-04 12:34:07 +0100

    day04

---
+++ src/Day04.kt
@@ -0,0 +1,32 @@
+fun main() {
+
+
+    val fileName = "day04.txt"
+    val testFileName = "day04_test.txt"
+    val input = FileUtil.getListOfLines(fileName);
+
+    //Comparing strings does not work because comparing "1" or "23" with  "234567891011" would trigger true
+    //ALternative comparing "1,2" with "21,22" would return true
+    //12,,13,,14
+
+    //Order matters
+    //1,2...
+    fun String.rangeToBytes():List<Byte>{
+        var bytes = mutableListOf<Byte>()
+        val (start, end) = this.split("-")
+        for(i in start.toInt()..end.toInt()){
+            bytes.add(i.toByte())
+        }
+        return bytes
+    }
+    fun List<List<Byte>>.contains():Boolean{
+        val (first,second) =this
+        return first.containsAll(second) || second.containsAll(first)
+    }
+
+
+    val digits = input.map { it -> it.split(",").map { it.rangeToBytes() }}.map { it.contains() }.count { it }
+    println(digits)
+
+}
+
