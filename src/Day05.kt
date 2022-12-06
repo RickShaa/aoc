29d4820b494f7540a09d7cf5582aa6697f80183f
RickShaa <r.shaffer@gmx.de>
2022-12-06 20:45:23 +0100

    day05_part01

--- src/Day05.kt
+++ src/Day05.kt
@@ -1,4 +1,5 @@
-import java.util.Stack
+import java.util.*
+import kotlin.collections.ArrayDeque
 import kotlin.math.sign
 
 fun main() {
@@ -14,70 +15,67 @@ fun main() {
         return crates.toList()
     }
 
-    val stacks = cratesAndInstructions[0].split("\n")
+    val lines = cratesAndInstructions[0].split("\n")
         .map {
             it
-            .replace("    ", "[#]")
+            .replace("    ", "#")
             .replace(" ", "")
             .replace("[", "")
             .replace("]", "")
         }.map { it -> it.patchLastValue()}.take(8)
 
-    fun MutableMap<Int,MutableList<Char>>.reverseListEntries(){
-        for(entry in this.entries.iterator())
-            this[entry.key] = entry.value.reversed() as MutableList<Char>
-    }
+    lines.forEach { println(it) }
 
-    fun fillStacksWithCrates(stacks:List<List<Char>>):MutableMap<Int,MutableList<Char>>{
-        var filledStack:MutableMap<Int,MutableList<Char>> = mutableMapOf()
-        stacks.forEach { crates -> crates.forEachIndexed { index, c ->
+
+    fun createStacks(lines:List<List<Char>>):MutableMap<Int,ArrayDeque<Char>> {
+        var filledStack:MutableMap<Int,ArrayDeque<Char>> = mutableMapOf()
+        lines.forEach { line -> line.forEachIndexed { index, c ->
             val idx = index+1;
             if(c.toString() != "#"){
                 if(filledStack.contains(idx)){
-                    filledStack[idx]?.add(c)
+                    filledStack[idx]?.addFirst(c)
                 }else{
-                    filledStack[idx] = mutableListOf(c)
+                    filledStack[idx] = ArrayDeque(listOf(c))
                 }
             }
         }}
-        filledStack.reverseListEntries()
+
         return filledStack
     }
 
 
     fun createInstructions(text:List<String>, regex: Regex): List<Instruction> {
         return text
-            .map{ instruction -> regex.findAll(instruction).map { it.groupValues[1] }.joinToString()}
+            .map{ instruction -> regex.findAll(instruction).map { it.groupValues[0] }.joinToString()}
             .map { it.split(",").map{ it.trim().toInt()} }
             .map { item -> Instruction(item[0], item[1], item[2]) }
     }
 
     val instText = cratesAndInstructions[1].split("\n")
-    val instRegex = "([1-9])".toRegex()
+    val instRegex = "\\d{1,2}".toRegex()
     val instructions = createInstructions(instText,instRegex)
-    val stacksAndCrates = fillStacksWithCrates(stacks)
+    val stacks = createStacks(lines)
 
-
-
-    println(stacksAndCrates)
-    fun moveFromOriginToDestination(instruction: Instruction) {
-        val originStack = stacksAndCrates[instruction.originStack]
-        val destinationStack = stacksAndCrates[instruction.destinationStack]
-        if(originStack?.size != 0){
-            val removedElement = originStack?.removeAt(originStack.size -1)
-            if (removedElement != null) {
-                destinationStack?.add(removedElement)
-            }
+    fun getTopCrates(sortedStacks: MutableMap<Int, ArrayDeque<Char>>): String {
+        var result = ""
+        for(i in 1..9){
+            result+= sortedStacks.get(i)?.removeLast()
         }
-
+        return result
     }
 
+
+    fun moveFromOriginToDestination(instruction: Instruction) {
+        val originStack = stacks[instruction.originStack]
+        val destinationStack = stacks[instruction.destinationStack]
+        val removedElement = originStack?.removeLast()
+        if (removedElement != null) {
+            destinationStack?.addLast(removedElement)
+        }
+    }
     instructions.map { instruction -> repeat(instruction.numberOfCrates){moveFromOriginToDestination(instruction)} }
 
-
-    //println(stacks)
-    println(stacksAndCrates)
-
+    println(getTopCrates(stacks))
 }
 
 
