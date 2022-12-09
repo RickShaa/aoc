7d27c1f3cc631adf07721fbee8b2975132d4ff80
RickShaa <r.shaffer@gmx.de>
2022-12-09 15:30:59 +0100

    day 6 part1 and 2

---
+++ src/Day06.kt
@@ -0,0 +1,29 @@
+import java.util.*
+import kotlin.collections.ArrayDeque
+import kotlin.math.sign
+
+fun main() {
+    val fileName = "day06.txt"
+    val testFileName = "day06_test.txt"
+    val input = FileUtil.getTrimmedText(fileName);
+
+    fun isStarterPacket(packet:List<Char>):Boolean{
+        println(packet)
+        return packet.distinct().size == packet.size
+    }
+
+    fun List<Char>.findDistinctSequence(size:Byte): Int? {
+        var bufferEnd:Int? = null
+        for (i in this.indices){
+            bufferEnd = i + size
+            if(isStarterPacket(this.subList(i, bufferEnd ))){
+                break
+            }
+        }
+        return bufferEnd
+    }
+
+    val dataStream = input.toCharArray().toList();
+    println(dataStream.findDistinctSequence(14))
+
+}
