d48f2bec90aca92ed8b207c3816cd43b30e05046
RickShaa <r.shaffer@gmx.de>
2022-12-15 20:47:05 +0100

    day 10 part 1 and 2

---
+++ src/Day10.kt
@@ -0,0 +1,64 @@
+import java.util.*
+import kotlin.collections.ArrayDeque
+import kotlin.math.sign
+
+fun main() {
+    val fileName = "day10.txt"
+    val testFileName = "day10_test.txt"
+
+    fun String.getCPUInstruction():List<String>{
+        return this.split(" ")
+    }
+    fun calculateSignalStrength(cycle:Int, relevantCycles:MutableList<Int>, registerValue:Int):Int{
+        if(relevantCycles.size >= 1 && relevantCycles[0] == cycle){
+            val cycleNumber = relevantCycles.removeAt(0)
+            return registerValue * cycleNumber
+        }
+        return 0
+    }
+
+    // PART 2
+
+    fun getPixel(registerX:Int, pixelIdx:Int):String{
+        if(pixelIdx == registerX -2 || pixelIdx == registerX - 1 || pixelIdx == registerX ){
+            return "#"
+        }
+        return "."
+    }
+
+    fun drawPixel(pixels:MutableList<String>,currentCycle:Int,registerX:Int){
+        if(pixels.size<40){
+            pixels.add(getPixel(registerX,pixels.size -1))
+        }else if(pixels.size == 40){
+            println(pixels.joinToString(""))
+            pixels.removeAll(pixels)
+            pixels.add(getPixel(registerX,0))
+        }
+    }
+
+    val input = FileUtil.getListOfLines(fileName);
+    var x = 1;
+    val relevantCycle = mutableListOf(20,60,100,140,180,220)
+    var currentCycle = 0
+    var signalStrength = 0
+    var pixels = mutableListOf<String>()
+
+    for(i in input.indices){
+        val cpuInstruction = input[i].getCPUInstruction()
+        if(cpuInstruction.size == 1){
+            currentCycle++ //start cycle
+            drawPixel(pixels, currentCycle,x) // during cycle
+            //signalStrength+=calculateSignalStrength(currentCycle, relevantCycle, x)
+        }else{
+            for(i in 0..1){
+                //DRAWS PIXEL
+                currentCycle++ //start cycle
+                drawPixel(pixels,currentCycle,x) //during cycle
+                //signalStrength+=calculateSignalStrength(currentCycle, relevantCycle, x)
+            }
+            x+= cpuInstruction[1].toInt()
+        }
+    }
+    println(pixels.joinToString(""))
+    println(signalStrength)
+}
--- src/Day13.kt
+++ src/Day13.kt
@@ -5,7 +5,7 @@ fun main() {
 
     val fileName = "day13.txt"
     val testFileName = "day13_test.txt"
-    val input = FileUtil.getListOfLines(fileName);
+    val input = FileUtil.getListOfLines(testFileName);
 
     fun Any.isInt():Boolean{
         return this is Int
@@ -21,78 +21,45 @@ fun main() {
         return leftTuple.size -1
     }
 
-    fun handleIntComparison(leftList:List<Any>, rightList:List<Any>, x:Int, maxIndex:Int):Boolean{
-        val rightValue = rightList[x] as Int
-        val leftValue = leftList[x] as Int
-        val leftSideRanOutOfData = x == maxIndex && leftList.size < rightList.size
-        val rightSideRanOutOfData = x == maxIndex && leftList.size > rightList.size
-        if(leftValue  < rightValue){
-            println("CORRECT Compared $leftValue with right: $rightValue")
-            return true
-        }else if(leftValue > rightValue){
-            println("Order not correct lerft: $leftValue  right: $rightValue")
-            return false
-        }else if(leftSideRanOutOfData){
-            return true
-        }else if (rightSideRanOutOfData){
-            return false
-        }
+    fun compare(left:List<Any>, right:List<Any>):Boolean{
+            //CASE BOTH LISTS
+            if(left.isEmpty() && right.isNotEmpty()){
+                return true
+            }else if(left.isNotEmpty() && right.isEmpty()){
+                return false
+            }else {
+                //iterate through list
+                val max = minTupleEndIdx(left, right)
+                for (x in 0..max) {
+                    println("${left[x]}  ------ ${right[x]}")
+                    if (left[x].isInt() && right[x].isInt()) {
+                        val r = right[x] as Int
+                        val l = left[x] as Int
+                        if (r > l) {
+                            return true
+                        } else if (r < l) {
+                            return false
+                        }
+                    } else if (left[x].isInt() && right[x].isList()) {
+                        val l = left[x] as Int
+                        val r = right[x] as List<Any>
+                        return compare(listOf(l), r)
+                    }else if(left[x].isList() && right[x].isList()){
+                       compare(left[x] as List<Any>, right[x] as List<Any>)
+                    } else {
+                        val r = right[x] as Int
+                        val l = left[x] as List<Any>
+                        return compare(l, listOf(r))
+                    }
+                }
+            }
         return false
     }
 
-    fun compareList(tuple: List<List<Any>>):Boolean{
-        val leftList = tuple[0]
-        val rightList = tuple[1]
-        var isInOrder = false;
-        if(leftList.isEmpty() && rightList.isNotEmpty()){
-            return true
-        }else if(leftList.isNotEmpty() && rightList.isEmpty()){
-            return false
-        }
-
-        val minIndex = minTupleEndIdx(leftList,rightList)
-        for(x in 0..minIndex){
-            if(!isInOrder){
-                if(leftList[x].isInt() && rightList[x].isInt()){
-                    isInOrder = handleIntComparison(leftList, rightList, x, minIndex)
-                    //exit for loop as soon as no longer in right order
-                }else if(leftList[x].isList() && rightList[x].isList()){
-                    isInOrder = compareList(listOf(leftList[x] as List<Any>, rightList[x] as List<Any>))
-                }else{
-                    isInOrder = if(leftList[x].isInt()){
-                        val listifyInt = listOf(leftList[x] as Int)
-                        val list = rightList[x] as List<Any>
-                        compareList(listOf(listifyInt, list))
-                    }else{
-                        val listifyInt = listOf(rightList[x] as Int)
-                        val list = leftList[x] as List<Any>
-                        compareList(listOf(list, listifyInt))
-                    }
-                }
-            }else{
-                break
-            }
-        }
-        return isInOrder
+    val chunkedInput = input.mapNotNull { it.takeIf { s: String -> s.isNotEmpty() } }
+    for(x in chunkedInput.indices step 2){
+        val left = Klaxon().parseArray<Any>(chunkedInput[x])!!
+        val right = Klaxon().parseArray<Any>(chunkedInput[x+1])!!
+        println(compare(left,right))
     }
-
-    val chunkedInput = input.mapNotNull { it.takeIf { s: String -> s.isNotEmpty() } }.chunked(2)
-    val parsedInput = chunkedInput
-        .map { pairs -> pairs
-            .map { packet -> Klaxon().parseArray<Any>(packet)!! }}
-
-
-    fun sumOfIndicies(): Int {
-        var sum = 0
-        val orderedPackets = parsedInput.map { compareList(it)}
-        println(orderedPackets)
-        for(x in orderedPackets.indices){
-            if(orderedPackets[x]){
-                sum+=x+1
-            }
-        }
-        return sum
-    }
-
-    println(sumOfIndicies())
 }
