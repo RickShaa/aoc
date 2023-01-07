158a693cb66578dc8db3b4a2e65e1c6302896568
RickShaa <r.shaffer@gmx.de>
2023-01-07 20:20:07 +0100

    optimized AOC 6

--- src/Day06.kt
+++ src/Day06.kt
@@ -5,9 +5,12 @@ import kotlin.math.sign
 fun main() {
     val fileName = "day06.txt"
     val testFileName = "day06_test.txt"
-    val input = FileUtil.getTrimmedText(fileName);
+    val input = FileUtil.getTrimmedText(testFileName);
 
-    fun isStarterPacket(packet:List<Char>):Boolean{
+    /**
+     * Initial solution
+     */
+    /*fun isStarterPacket(packet:List<Char>):Boolean{
         //transform to set, because set does not allow duplicates
         return packet.toSet().size == packet.size
     }
@@ -21,9 +24,39 @@ fun main() {
             }
         }
         return bufferEnd
-    }
+    }*/
 
+    
+
+    /**
+     * Optimized Solution 1
+     */
     val dataStream = input.toCharArray().toList();
-    println(dataStream.findDistinctSequence(4))
 
+
+    /*fun distinctSequence(seqLen:Int): Int? {
+        val windowedData =  dataStream.windowed(seqLen)
+        var idx:Int? = null
+        for ((offset,data) in windowedData.withIndex()) {
+            if (data.toHashSet().size == data.size) {
+                idx = offset + seqLen
+                break;
+            }
+        }
+        return idx;
+    }*/
+
+
+
+    /*
+        Optimized Solution 2
+
+     */
+   fun distinctSequence(seqLen:Int): Any {
+        val windowedData = dataStream.windowed(seqLen)
+        val index = windowedData.indexOfFirst{window -> window.toHashSet().size == window.size}
+        return index + seqLen
+   }
+
+    println(distinctSequence(4))
 }
